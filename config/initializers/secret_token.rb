832ef835a856d6fe3c0ff2013e411ed2134409eb
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-08-05 14:16:51 -0700

    add rails new /script

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyMtn::Application.config.secret_key_base = 'f90b95bba9b80d017bf6fc08955d84feef5bfc2d7d0cdf9c8f624729a0073fa9720ef7f4c91c45217c8f7e65e24e12277ddaa61f678bead7e270b99e4d16fd69'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubyMtn::Application.config.session_store :cookie_store, key: '_ruby_mtn_session'
